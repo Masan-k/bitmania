2157e71499c2ee3cb48ddedff492d86774c6ca29
Masan-k <uronti@hotmail.co.jp>
2023-12-08 14:11:36 +0900

    up

---
+++ www/localUpdate.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cd /home/userX260/work/bitmania/www
+cp 01_8beat.txt /var/www/html/bitmania
+cp 99_testScore.html /var/www/html/bitmania
+echo "beatmania files copied from work to localhost."
