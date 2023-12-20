67a9083764d6375f04dac9f8a57b5e9b7104b3f3
Masan-k <uronti@hotmail.co.jp>
2023-12-20 21:09:26 +0900

    input music ok

--- www/localUpdate.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cd /home/userX260/work/bitmania/www
-cp 01_8beat.txt /var/www/html/bitmania
-cp 02_8beat.txt /var/www/html/bitmania
-cp 03_8beat.txt /var/www/html/bitmania
-cp 04_8beat.txt /var/www/html/bitmania
-cp mainClick.mp3 /var/www/html/bitmania
-cp subClick.mp3 /var/www/html/bitmania
-cp 99_testScore.html /var/www/html/bitmania
